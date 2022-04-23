8fc24cc8a3040313d2b8ef85f21310d82bf38997
brucetrindade <brucetrindade2000@gmail.com>
2022-04-23 15:31:04 -0300

    Nav Setup

--- app/src/main/java/com/example/borutocharacters/MainActivity.kt
+++ app/src/main/java/com/example/borutocharacters/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.borutocharacters
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.borutocharacters.navigation.SetupNavGraph
+import com.example.borutocharacters.ui.theme.ui.theme.BorutoCharactersTheme
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            BorutoCharactersTheme {
+                navController = rememberNavController()
+                SetupNavGraph(navController = navController)
+
+            }
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/borutocharacters/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.borutocharacters.util
+
+object Constants {
+
+    const val DETAILS_ARGUMENT_KEY = "heroId"
+
+
+}
